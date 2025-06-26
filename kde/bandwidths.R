02d00d864fef0bd6ccae0e532352b25b097b65b5
tksiu <44538003+tksiu@users.noreply.github.com>
2025-06-26 03:36:32 -0300

    update

--- kde/bandwidths.R
+++ kde/bandwidths.R
@@ -10,16 +10,19 @@ require(spatstat)
 
 
 
-#   set the maximum threshold allowed
+#   get pairwise distance between any pair of stations
 pdist = pairdist(
     superimpose(pa_points, fem_points)
 )
+#   set the maximum bandwidth threshold allowed as the half distance from the nearest monitor (IQR rule of outliers)
 maxdist = as.numeric(
   quantile(apply(pdist, 1, function(x) min(x[x > 0])) / 2, 0.75) * 111 + 
   1.5 * (quantile(apply(pdist, 1, function(x) min(x[x > 0])) / 2, 0.75) * 111 - quantile(apply(pdist, 1, function(x) min(x[x > 0])) / 2, 0.25) * 111)
 )
-ngrids = ceiling(maxdist)
-ndist = ngrids * 0.01
+#   in km (rounding up to integer)
+ndist = ceiling(maxdist)
+#   in numer of grids
+ndist = ndist * 0.01
 
 
 #   set the cut-off value of the percentage variation in PM2.5 concentration
@@ -34,51 +37,58 @@ bandwidth_selection = function(coord) {
 
     for (i in 1:dim(coord)[1]) {
     
-    pt_lat = coord$Lat[i]
-    pt_lon = coord$Lon[i]
+        pt_lat = coord$Lat[i]
+        pt_lon = coord$Lon[i]
     
-    midpoint_lat = 0.5 * (ceiling(pt_lat*100)/100 + floor(pt_lat*100)/100)
-    midpoint_lon = 0.5 * (ceiling(pt_lon*100)/100 + floor(pt_lon*100)/100)
+        #  based on centroid of the grids
+        midpoint_lat = 0.5 * (ceiling(pt_lat*100)/100 + floor(pt_lat*100)/100)
+        midpoint_lon = 0.5 * (ceiling(pt_lon*100)/100 + floor(pt_lon*100)/100)
     
-    if (pt_lon > midpoint_lon) {
-        adj_a = -0.005
-        adj_b = 0
-    } else if (pt_lon < midpoint_lon) {
-        adj_a = 0
-        adj_b = 0.005
-    } else {
-        adj_a = -0.005
-        adj_b = 0.005
-    }
-    if (pt_lat > midpoint_lat) {
-        adj_c = -0.005
-        adj_d = 0
-    } else if (pt_lat < midpoint_lat) {
-        adj_c = 0
-        adj_d = 0.005
-    } else {
-        adj_c = -0.005
-        adj_d = 0.005
-    }
+        #  buffering to ensure the same searching window size for each grid
+        if (pt_lon > midpoint_lon) {
+            adj_a = -0.005
+            adj_b = 0
+        } else if (pt_lon < midpoint_lon) {
+            adj_a = 0
+            adj_b = 0.005
+        } else {
+            adj_a = -0.005
+            adj_b = 0.005
+        }
+        if (pt_lat > midpoint_lat) {
+            adj_c = -0.005
+            adj_d = 0
+        } else if (pt_lat < midpoint_lat) {
+            adj_c = 0
+            adj_d = 0.005
+        } else {
+            adj_c = -0.005
+            adj_d = 0.005
+        }
     
-    extract_pm25 = ncvar_get(nc_data, "PM25")[
-        which( nc_data$dim$longitude$vals >= pt_lon - ndist + adj_a & nc_data$dim$longitude$vals <= pt_lon + ndist + adj_b ), 
-        which( nc_data$dim$latitude$vals >= pt_lat - ndist + adj_c & nc_data$dim$latitude$vals <= pt_lat + ndist + adj_d )
-    ]
+        #  extract the searching window of the grid
+        extract_pm25 = ncvar_get(nc_data, "PM25")[
+            which( nc_data$dim$longitude$vals >= pt_lon - ndist + adj_a & nc_data$dim$longitude$vals <= pt_lon + ndist + adj_b ), 
+            which( nc_data$dim$latitude$vals >= pt_lat - ndist + adj_c & nc_data$dim$latitude$vals <= pt_lat + ndist + adj_d )
+        ]
     
-    centroid_idx = (dim(extract_pm25)[1] + 1) / 2
+        #  get the central point (current grid)
+        centroid_idx = (dim(extract_pm25)[1] + 1) / 2
     
-    gradient_pm25 = abs((extract_pm25 - extract_pm25[centroid_idx, centroid_idx]) / extract_pm25[centroid_idx, centroid_idx] * 100)
-    mask_gradient_pm25 = which( gradient_pm25 > pm25_percent_diff, arr.ind = T)
-    mask_gradient_pm25 = as.data.frame(mask_gradient_pm25)
-    mask_gradient_pm25$euclidean_dist = mapply(function(a,b) {sqrt((a - centroid_idx)^2 + (b - centroid_idx)^2)}, 
-                                                mask_gradient_pm25$row, mask_gradient_pm25$col)
-    
-    if (length(mask_gradient_pm25$euclidean_dist) > 0) {
-        bandwidths[i] = min(mask_gradient_pm25$euclidean_dist, maxdist)
-    } else {
-        bandwidths[i] = maxdist
-    }
+        #  consider only surrounding grids within the searching window with PM2.5 conc. difference exceeding 10% from current grid
+        gradient_pm25 = abs((extract_pm25 - extract_pm25[centroid_idx, centroid_idx]) / extract_pm25[centroid_idx, centroid_idx] * 100)
+        mask_gradient_pm25 = which( gradient_pm25 > pm25_percent_diff, arr.ind = T)
+        mask_gradient_pm25 = as.data.frame(mask_gradient_pm25)
+        mask_gradient_pm25$euclidean_dist = mapply(function(a,b) {sqrt((a - centroid_idx)^2 + (b - centroid_idx)^2)}, 
+                                                    mask_gradient_pm25$row, mask_gradient_pm25$col)
+            
+        #  select bandwidth to be the smallest distance between current grid and the retained grids (>10% PM2.5 conc. difference)
+        #  select maximum bandwidth threshold if no surrounding grid in the searching window satisfied the criterion
+        if (length(mask_gradient_pm25$euclidean_dist) > 0) {
+            bandwidths[i] = min(mask_gradient_pm25$euclidean_dist, maxdist)
+        } else {
+            bandwidths[i] = maxdist
+        }
     
     }
 
