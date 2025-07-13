f196a3368da042cd37fbf01e65808d8ad134d765
tksiu <44538003+tksiu@users.noreply.github.com>
2025-07-13 01:57:27 -0300

    Update

--- env_justice/census_zonal_stats.R
+++ env_justice/census_zonal_stats.R
@@ -41,82 +41,89 @@ quintiles = function(x) {
 ###  Extract Zonal Statistics ###
 if (sys.nframe() == 0) {
 
-    sp_tw_kde_zonal = exact_extract(raster(pa_sp_den_tw), da$geometry, "mean")
-    fem_sp_tw_kde_zonal = exact_extract(raster(fem_sp_den_tw), da$geometry, "mean")
-    pm25_zonal = exact_extract(pm25_crop, da$geometry, "mean")
-
-    da$sp_tw_kde = sp_tw_kde_zonal
-    da$fem_sp_tw_kde = fem_sp_tw_kde_zonal
-
-    da$sp_tw_kde[is.na(da$sp_tw_kde)] = 0
-    da$fem_sp_tw_kde[is.na(da$fem_sp_tw_kde)] = 0
-
-    da$total_sp_tw_kde = da$sp_tw_kde + da$fem_sp_tw_kde
-    da$pm25_3yr_mean = pm25_zonal
-
-
-    ###  Merge/Fusion of Datasets ###
-
-    da = merge(da, can_cimd, by="DAUID", all.x=T)
-    da = merge(da, prov_cimd[, c(1, 4:length(can_cimd))], by="DAUID", all.x=T)
-
-    da$Region = ifelse(da$PRUID %in% c("10","11","12","13"), "Atlantic", ifelse(da$PRUID == "35", "Ontario", "Quebec"))
-
-
-    ###  Extract Quintiles ###
-
-    da$total_sp_tw_kde.ecdf = ecdf(da$total_sp_tw_kde)(da$total_sp_tw_kde)
-    da$total_sp_tw_kde.Quintiles = sapply(da$total_sp_tw_kde.ecdf, quintiles)
-
-    da = da %>% group_by(Region) %>% mutate(total_sp_tw_kde.group_ecdf = ecdf(total_sp_tw_kde)(total_sp_tw_kde))
-    da$total_sp_tw_kde.group_Quintiles = sapply(da$total_sp_tw_kde.group_ecdf, quintiles)
-
-    da$pm25_3yr_mean.ecdf = ecdf(da$pm25_3yr_mean)(da$pm25_3yr_mean)
-    da$pm25_3yr_mean.Quintiles = sapply(da$pm25_3yr_mean.ecdf, quintiles)
-
-    da = da %>% group_by(Region) %>% mutate(pm25_3yr_mean.group_ecdf = ecdf(pm25_3yr_mean)(pm25_3yr_mean))
-    da$pm25_3yr_mean.group_Quintiles = sapply(da$pm25_3yr_mean.group_ecdf, quintiles)
-
-
-    ###  Largest quintile among the four CIMD dimensions  ###
-
-    da$can_Deprivation.Quintiles = apply(cbind(da$can_Residential.instability.Quintiles, 
-                                            da$can_Economic.dependency.Quintiles, 
-                                            da$can_Situational.vulnerability.Quintiles, 
-                                            da$`can_Ethno-cultural.composition.Quintiles`), 1, max, na.rm=T)
-
-    da$prov_Deprivation.Quintiles = apply(cbind(da$prov_Residential.instability.Quintiles, 
-                                                da$prov_Economic.dependency.Quintiles, 
-                                                da$prov_Situational.vulnerability.Quintiles, 
-                                                da$`prov_Ethno-cultural.composition.Quintiles`), 1, max, na.rm=T)
-
-    da$can_Deprivation.Quintiles[da$can_Deprivation.Quintiles == -Inf] = NA
-    da$prov_Deprivation.Quintiles[da$prov_Deprivation.Quintiles == -Inf] = NA
-
-
-    ###  Any of the four CIMD dimensions falling in the top quintile  ###
+   sp_tw_kde_zonal = exact_extract(raster(pa_sp_den_tw), da$geometry, "mean")
+   fem_sp_tw_kde_zonal = exact_extract(raster(fem_sp_den_tw), da$geometry, "mean")
+   pm25_zonal = exact_extract(pm25_crop, da$geometry, "mean")
    
-    da$can_Deprivation_top_quin_count = apply(cbind(da$can_Residential.instability.Quintiles, 
-                                                    da$can_Economic.dependency.Quintiles, 
-                                                    da$can_Situational.vulnerability.Quintiles, 
-                                                    da$`can_Ethno-cultural.composition.Quintiles`), 1, function(x) length(x[x == 5]))
+   da$sp_tw_kde = sp_tw_kde_zonal
+   da$fem_sp_tw_kde = fem_sp_tw_kde_zonal
+   
+   da$sp_tw_kde[is.na(da$sp_tw_kde)] = 0
+   da$fem_sp_tw_kde[is.na(da$fem_sp_tw_kde)] = 0
+   
+   da$total_sp_tw_kde = da$sp_tw_kde + da$fem_sp_tw_kde
+   da$pm25_3yr_mean = pm25_zonal
+   
+   
+   ###  Merge/Fusion of Datasets ###
+   
+   da = merge(da, can_cimd, by="DAUID", all.x=T)
+   da = merge(da, prov_cimd[, c(1, 4:length(can_cimd))], by="DAUID", all.x=T)
+   
+   da$Region = ifelse(da$PRUID %in% c("10","11","12","13"), "Atlantic", ifelse(da$PRUID == "35", "Ontario", "Quebec"))
+   
+   
+   ###  Extract Quintiles ###
+   
+   da$total_sp_tw_kde.ecdf = ecdf(da$total_sp_tw_kde)(da$total_sp_tw_kde)
+   da$total_sp_tw_kde.Quintiles = sapply(da$total_sp_tw_kde.ecdf, quintiles)
+   
+   da = da %>% group_by(Region) %>% mutate(total_sp_tw_kde.group_ecdf = ecdf(total_sp_tw_kde)(total_sp_tw_kde))
+   da$total_sp_tw_kde.group_Quintiles = sapply(da$total_sp_tw_kde.group_ecdf, quintiles)
+   
+   da$pm25_3yr_mean.ecdf = ecdf(da$pm25_3yr_mean)(da$pm25_3yr_mean)
+   da$pm25_3yr_mean.Quintiles = sapply(da$pm25_3yr_mean.ecdf, quintiles)
+   
+   da = da %>% group_by(Region) %>% mutate(pm25_3yr_mean.group_ecdf = ecdf(pm25_3yr_mean)(pm25_3yr_mean))
+   da$pm25_3yr_mean.group_Quintiles = sapply(da$pm25_3yr_mean.group_ecdf, quintiles)
+   
+   
+   ###  Largest quintile among the four CIMD dimensions  ###
+   
+   da$can_Deprivation.Quintiles = apply(cbind(da$can_Residential.instability.Quintiles, 
+                                         da$can_Economic.dependency.Quintiles, 
+                                         da$can_Situational.vulnerability.Quintiles, 
+                                         da$`can_Ethno-cultural.composition.Quintiles`), 1, max, na.rm=T)
+   
+   da$prov_Deprivation.Quintiles = apply(cbind(da$prov_Residential.instability.Quintiles, 
+                                             da$prov_Economic.dependency.Quintiles, 
+                                             da$prov_Situational.vulnerability.Quintiles, 
+                                             da$`prov_Ethno-cultural.composition.Quintiles`), 1, max, na.rm=T)
+   
+   da$can_Deprivation.Quintiles[da$can_Deprivation.Quintiles == -Inf] = NA
+   da$prov_Deprivation.Quintiles[da$prov_Deprivation.Quintiles == -Inf] = NA
+   
+   
+   ###  Any of the four CIMD dimensions falling in the top quintile  ###
+   
+   da$can_Deprivation_top_quin_count = apply(cbind(da$can_Residential.instability.Quintiles, 
+                                                 da$can_Economic.dependency.Quintiles, 
+                                                 da$can_Situational.vulnerability.Quintiles, 
+                                                 da$`can_Ethno-cultural.composition.Quintiles`), 1, function(x) length(x[x == 5]))
+   
+   da$prov_Deprivation_top_quin_count = apply(cbind(da$prov_Residential.instability.Quintiles, 
+                                                 da$prov_Economic.dependency.Quintiles, 
+                                                 da$prov_Situational.vulnerability.Quintiles, 
+                                                 da$`prov_Ethno-cultural.composition.Quintiles`), 1, function(x) length(x[x == 5]))
+   
+   da$can_Deprivation_category = ifelse(da$can_Deprivation_top_quin_count > 1, "multiple", 
+                                     ifelse(da$can_Deprivation_top_quin_count == 1, "single", "none"))
+                                     
+   da$prov_Deprivation_category = ifelse(da$prov_Deprivation_top_quin_count > 1, "multiple", 
+                                     ifelse(da$prov_Deprivation_top_quin_count == 1, "single", "none"))
+   
+   da$coverage_status = ifelse(da$fem_sp_tw_kde > 0, "Covered by FEM", 
+                         ifelse(da$fem_sp_tw_kde == 0 & da$total_sp_tw_kde > 0, "Enhanced by PurpleAir", "Not covered"))
+   
+   da$percent_enhanced_by_pa_sensor = da$total_sp_tw_kde / (da$fem_sp_tw_kde + 0.0001) * 100
+   da$percent_enhanced_by_pa_sensor = ifelse(log(da$percent_enhance_pa_sensor) == -Inf, NA, log(da$percent_enhance_pa_sensor))
 
-    da$prov_Deprivation_top_quin_count = apply(cbind(da$prov_Residential.instability.Quintiles, 
-                                                    da$prov_Economic.dependency.Quintiles, 
-                                                    da$prov_Situational.vulnerability.Quintiles, 
-                                                    da$`prov_Ethno-cultural.composition.Quintiles`), 1, function(x) length(x[x == 5]))
-
-    da$can_Deprivation_category = ifelse(da$can_Deprivation_top_quin_count > 1, "multiple", 
-                                        ifelse(da$can_Deprivation_top_quin_count == 1, "single", "none"))
-                                        
-    da$prov_Deprivation_category = ifelse(da$prov_Deprivation_top_quin_count > 1, "multiple", 
-                                        ifelse(da$prov_Deprivation_top_quin_count == 1, "single", "none"))
-
-    da$coverage_status = ifelse(da$fem_sp_tw_kde > 0, "Covered by FEM", 
-                            ifelse(da$fem_sp_tw_kde == 0 & da$total_sp_tw_kde > 0, "Enhanced by PurpleAir", "Not covered"))
-
-    da$percent_enhanced_by_pa_sensor = da$total_sp_tw_kde / (da$fem_sp_tw_kde + 0.0001) * 100
-    da$percent_enhanced_by_pa_sensor = ifelse(log(da$percent_enhance_pa_sensor) == -Inf, NA, log(da$percent_enhance_pa_sensor))
+                                              
+   ###   Identify monitoring gaps of concern due to exceedances of 3-year averaged surface PM2.5 compared to WHO annual guideline (5 ug/m3)
+                                              
+   da$pm25_exceedance = ifelse(da$pm25_3yr_mean > 5, 1, 0)
+   da$total_kde_gap_with_pm25_exceedance = ifelse(da$pm25_3yr_mean > 5 & da$total_sp_tw_kde == 0, 1, 0)
+   da$fem_kde_gap_with_pm25_exceedance = ifelse(da$pm25_3yr_mean > 5 & da$fem_sp_tw_kde == 0, 1, 0)
 
 } else {
 
