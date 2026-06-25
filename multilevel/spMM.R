a0dc248ee9be08b544ca597e0677220752ebba51
tksiu <44538003+tksiu@users.noreply.github.com>
2026-06-25 02:45:11 -0300

    Update spMM.R

--- multilevel/spMM.R
+++ multilevel/spMM.R
@@ -13,10 +13,79 @@ da$adj_total_sp_tw_kde.Quintiles_binary = ifelse(da$adj_total_sp_tw_kde.Quintile
 ct$adj_total_sp_tw_kde.Quintiles_binary = ifelse(ct$adj_total_sp_tw_kde.Quintiles == 1, 1, 0)
 
 
+##  mixed-effects model without spatial terms
+
+cimd_logit = lme4::glmer(adj_total_sp_tw_kde.Quintiles_binary ~ 
+                           pm25_3yr_mean + 
+                           sqrt(sqrt(sqrt(npri_pm25_density))) +
+                           can_Residential.instability.Scores + 
+                           can_Economic.dependency.Scores + 
+                           can_Ethnocultural.composition.Scores + 
+                           can_Situational.vulnerability.Scores + 
+                           (1 + 
+                              pm25_3yr_mean + 
+                              sqrt(sqrt(sqrt(npri_pm25_density))) +
+                              can_Residential.instability.Scores + 
+                              can_Economic.dependency.Scores + 
+                              can_Ethnocultural.composition.Scores + 
+                              can_Situational.vulnerability.Scores | Province
+                           ),
+                        data = subset(da, !is.na(adj_total_sp_tw_kde.Quintiles)),
+                        family = binomial(),
+                        control = lme4::glmerControl(optCtrl=list(maxfun=1e6), calc.derivs = FALSE), nAGQ=0)
+
+pwd_logit = lme4::glmer(adj_total_sp_tw_kde.Quintiles_binary ~ 
+                           pm25_3yr_mean + 
+                           sqrt(sqrt(sqrt(npri_pm25_density))) +
+                           phy_perc +
+                           psy_perc +
+                           (1 + 
+                              pm25_3yr_mean + 
+                              sqrt(sqrt(sqrt(npri_pm25_density))) +
+                              phy_perc +
+                              psy_perc | PRUID / CMANAME
+                           ),
+                        data = subset(ct, !is.na(adj_total_sp_tw_kde.Quintiles)),
+                        family = binomial(),
+                        control = lme4::glmerControl(optCtrl=list(maxfun=1e6), calc.derivs = FALSE), nAGQ=0)
+
+
+pwd_logit_a = lme4::glmer(adj_total_sp_tw_kde.Quintiles_binary ~ 
+                            pm25_3yr_mean + 
+                            sqrt(sqrt(sqrt(npri_pm25_density))) +
+                            chd_phy_perc +
+                            chd_psy_perc +
+                            (1 + 
+                               pm25_3yr_mean + 
+                               sqrt(sqrt(sqrt(npri_pm25_density))) +
+                               chd_phy_perc +
+                               chd_psy_perc | PRUID / CMANAME
+                            ),
+                          data = subset(ct, !is.na(adj_total_sp_tw_kde.Quintiles)),
+                          family = binomial(),
+                          control = lme4::glmerControl(optCtrl=list(maxfun=1e6), calc.derivs = FALSE), nAGQ=0)
+
+pwd_logit_b = lme4::glmer(adj_total_sp_tw_kde.Quintiles_binary ~ 
+                             pm25_3yr_mean + 
+                             sqrt(sqrt(sqrt(npri_pm25_density))) +
+                             eld_phy_perc +
+                             eld_psy_perc +
+                             (1 + 
+                                pm25_3yr_mean + 
+                                sqrt(sqrt(sqrt(npri_pm25_density))) +
+                                eld_phy_perc +
+                                eld_psy_perc | PRUID / CMANAME
+                             ),
+                           data = subset(ct, !is.na(adj_total_sp_tw_kde.Quintiles)),
+                           family = binomial(),
+                           control = lme4::glmerControl(optCtrl=list(maxfun=1e6), calc.derivs = FALSE), nAGQ=0)
+
+
+
 #  create spatial lag term at DA level to account for the spatial autocorrelation in modelling the disparities
 #  weighted by Queen Contiguity
 
-geom_da = st_make_valid(subset(da, rownames(da) %in% rownames(cimd_logit_4@frame))$geometry)
+geom_da = st_make_valid(subset(da, rownames(da) %in% rownames(cimd_logit@frame))$geometry)
 
 ##  1) Queen contiguity
 queen_da = poly2nb(geom_da, queen = TRUE)
@@ -34,15 +103,6 @@ knn_da_w = nb2listw(knn_da, style = "W", zero.policy = TRUE)
 
 names(da) <- gsub("-", "", names(da))
 
-da$Province <- numFactor(as.factor(da$Province))
-da$ProvinceID <- as.numeric(da$Province)
-da$centroid = st_centroid(st_make_valid(da$geometry))
-da$x_coord = sapply(da$centroid, function(x) st_coordinates(x)[, "X"])
-da$y_coord = sapply(da$centroid, function(x) st_coordinates(x)[, "Y"])
-da$pos = numFactor(scale(da$x_coord), scale(da$y_coord))
-da$pos_group = factor(rep(1, nrow(da)))
-
-
 ##  generate spatially lagged variables with Queen contiguity weighting matrix
 
 da_processed = da %>% ungroup()
@@ -58,18 +118,67 @@ for (f in c("adj_total_sp_tw_kde.Quintiles_binary",
 }
 
 
+
 #  create spatial lag term at CT level to account for the spatial autocorrelation in modelling the disparities
 #  weighted by Queen Contiguity
 
-geom_ct = st_make_valid(subset(ct, rownames(ct) %in% rownames(pwd_logit_6@frame))$geometry)
+geom_ct = st_make_valid(subset(ct, rownames(ct) %in% rownames(pwd_logit@frame))$geometry)
+geom_ct_a = st_make_valid(subset(ct, rownames(ct) %in% rownames(pwd_logit_a@frame))$geometry)
+geom_ct_b = st_make_valid(subset(ct, rownames(ct) %in% rownames(pwd_logit_b@frame))$geometry)
+
+##  1) Queen's contiguity
 queen_ct = poly2nb(geom_ct, queen = TRUE)
 queen_ct_w = nb2listw(queen_ct, style = "W", zero.policy = TRUE)
 
-ct$centroid = st_centroid(st_make_valid(ct$geometry))
-ct$x_coord = sapply(ct$centroid, function(x) st_coordinates(x)[, "X"])
-ct$y_coord = sapply(ct$centroid, function(x) st_coordinates(x)[, "Y"])
-ct$pos = numFactor(scale(ct$x_coord), scale(ct$y_coord))
-ct$pos_group = factor(rep(1, nrow(ct)))
+queen_ct_a = poly2nb(geom_ct_a, queen = TRUE)
+queen_ct_a_w = nb2listw(queen_ct_a, style = "W", zero.policy = TRUE)
+
+queen_ct_b = poly2nb(geom_ct_b, queen = TRUE)
+queen_ct_b_w = nb2listw(queen_ct_b, style = "W", zero.policy = TRUE)
+
+##  2) Buffer distance (5 km)
+distnb_ct = dnearneigh(st_coordinates(st_centroid(geom_ct)), d1 = 0, d2 = 0.05)
+distnb_ct_w = nb2listwdist(distnb_ct, as(st_centroid(geom_ct), "Spatial"), type = "idw", style = "B", zero.policy = TRUE)
+
+distnb_ct_a = dnearneigh(st_coordinates(st_centroid(geom_ct_a)), d1 = 0, d2 = 0.05)
+distnb_ct_a_w = nb2listwdist(distnb_ct_a, as(st_centroid(geom_ct_a), "Spatial"), type = "idw", style = "B", zero.policy = TRUE)
+
+distnb_ct_b = dnearneigh(st_coordinates(st_centroid(geom_ct_b)), d1 = 0, d2 = 0.05)
+distnb_ct_b_w = nb2listwdist(distnb_ct_b, as(st_centroid(geom_ct_b), "Spatial"), type = "idw", style = "B", zero.policy = TRUE)
+
+##  3) k-NN neighbours (k = 5, setting a max. buffer = 25 km, because CMAs are disconnected, tracts at the edges of each CMA should be separated from other CMA if neighbours < 5)
+filter_nb = function(nb, dist, cutoff) {
+  valid_nb = nb[dist < cutoff]
+  if (length(valid_nb) == 0) {
+    return(as.integer(0))
+  } else {
+    return(as.integer(valid_nb))
+  }
+}
+
+knn_ct = knearneigh(st_coordinates(st_centroid(geom_ct)), k = 5)
+knn_ct = knn2nb(knn_ct)
+knn_ct_dist = nbdists(knn_ct, st_coordinates(st_centroid(geom_ct)))
+knn_ct_adj = mapply(filter_nb, knn_ct, knn_ct_dist, 0.05)
+class(knn_ct_adj) <- "nb"
+knn_ct_w = nb2listw(knn_ct_adj, style = "W", zero.policy = TRUE)
+
+knn_ct_a = knearneigh(st_coordinates(st_centroid(geom_ct_a)), k = 5)
+knn_ct_a = knn2nb(knn_ct_a)
+knn_ct_a_dist = nbdists(knn_ct_a, st_coordinates(st_centroid(geom_ct_a)))
+knn_ct_a_adj = mapply(filter_nb, knn_ct_a, knn_ct_a_dist, 0.05)
+class(knn_ct_a_adj) <- "nb"
+knn_ct_a_w = nb2listw(knn_ct_a_adj, style = "W", zero.policy = TRUE)
+
+knn_ct_b = knearneigh(st_coordinates(st_centroid(geom_ct_b)), k = 5)
+knn_ct_b = knn2nb(knn_ct_b)
+knn_ct_b_dist = nbdists(knn_ct_b, st_coordinates(st_centroid(geom_ct_b)))
+knn_ct_b_adj = mapply(filter_nb, knn_ct_b, knn_ct_b_dist, 0.05)
+class(knn_ct_b_adj) <- "nb"
+knn_ct_b_w = nb2listw(knn_ct_b_adj, style = "W", zero.policy = TRUE)
+
+
+##  generate spatially lagged variables with Queen contiguity weighting matrix
 
 ct_processed_pwd = ct %>% ungroup()
 for (f in c("adj_total_sp_tw_kde.Quintiles_binary",
